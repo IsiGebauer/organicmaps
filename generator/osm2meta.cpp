82540ff9b4c9e6a673099505b5381f9c9a73e144
IsiGebauer <isigebauer0@gmail.com>
2025-12-29 23:00:05 +0100

    added function to validate the mtb:scale

--- generator/osm2meta.cpp
+++ generator/osm2meta.cpp
@@ -366,6 +366,21 @@ std::string MetadataTagProcessorImpl::ValidateAndFormat_outdoor_seating(std::str
   return {};
 }
 
+std::string MetadataTagProcessorImpl::ValidateAndFormat_mtb_scale(std::string v)
+{
+  int difficulty = std::stoi(v);
+  // mtb:score must be in between 0 (easiest)- 5 (hardest)
+  if (difficulty < 0 )
+  {
+    difficulty = 0;
+  }
+  else if (difficulty > 5)
+  {
+    difficulty = 5; // TODO (gebauer): according to https://wiki.openstreetmap.org/wiki/DE:Key:mtb:scale a sacle of 6 is possible and basically means unridable
+  }
+  return std::string("S").append(std::to_string(difficulty));
+}
+
 std::string MetadataTagProcessorImpl::ValidateAndFormat_duration(std::string const & v) const
 {
   if (!ftypes::IsWayWithDurationChecker::Instance()(m_params.m_types))
@@ -566,6 +581,7 @@ void MetadataTagProcessor::operator()(std::string const & k, std::string const &
   case Metadata::FMD_SELF_SERVICE: valid = ValidateAndFormat_self_service(v); break;
   case Metadata::FMD_OUTDOOR_SEATING: valid = ValidateAndFormat_outdoor_seating(v); break;
   case Metadata::FMD_NETWORK: valid = ValidateAndFormat_operator(v); break;
+  case Metadata::FMD_MTBSCALE: valid = ValidateAndFormat_mtb_scale(v); break;
   // Metadata types we do not get from OSM.
   case Metadata::FMD_CUISINE:
   case Metadata::FMD_DESCRIPTION:   // processed separately
